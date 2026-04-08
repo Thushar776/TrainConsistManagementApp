b4d32d06c6015fb80d67e5427037b145e7816f01
Thushar776 <thusharr77@gmail.com>
2026-04-08 15:45:53 +0530

    UC20

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -5,46 +5,32 @@ public class TrainConsistManagementApp {
     public static void main(String[] args) {
 
         System.out.println("====================================");
-        System.out.println("UC19 - Binary Search for Bogie ID");
+        System.out.println("UC20 - Exception Handling During Search");
         System.out.println("====================================");
 
-        String[] bogieIds = {"BG101", "BG205", "BG309", "BG412", "BG550"};
+        String[] bogieIds = {};   // empty array
 
-        System.out.println();
-        System.out.println("Sorted Bogie IDs:");
-        for (String id : bogieIds) {
-            System.out.println(id);
+        String key = "BG101";
+
+        if (bogieIds.length == 0) {
+            throw new IllegalStateException("No bogies available in train. Cannot perform search.");
         }
 
-        String key = "BG309";
         boolean found = false;
 
-        int low = 0;
-        int high = bogieIds.length - 1;
-
-        while (low <= high) {
-            int mid = (low + high) / 2;
-
-            int result = bogieIds[mid].compareTo(key);
-
-            if (result == 0) {
+        for (String id : bogieIds) {
+            if (id.equals(key)) {
                 found = true;
                 break;
-            } else if (result < 0) {
-                low = mid + 1;
-            } else {
-                high = mid - 1;
             }
         }
 
-        System.out.println();
         if (found) {
-            System.out.println("Bogie " + key + " found using Binary Search.");
+            System.out.println("Bogie " + key + " found.");
         } else {
             System.out.println("Bogie " + key + " NOT found.");
         }
 
-        System.out.println();
-        System.out.println("UC19 search completed...");
+        System.out.println("UC20 search completed...");
     }
 }
