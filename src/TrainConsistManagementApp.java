87390edc08c5d8f775f0e7430cacd11ac027dc26
Thushar776 <thusharr77@gmail.com>
2026-04-08 14:46:34 +0530

    UC2:

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,2 +1,52 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class TrainConsistManagementApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=== Train Consist Management App ===");
+        System.out.println("====================================");
+
+        List<String> consist = new ArrayList<>();
+
+        System.out.println();
+        System.out.println("Train initialized successfully...");
+        System.out.println("Initial Bogie Count : " + consist.size());
+        System.out.println("Current Train Consist : " + consist);
+        System.out.println();
+        System.out.println("System ready for operations...");
+
+        System.out.println();
+        System.out.println("====================================");
+        System.out.println("UC2 - Add Passenger Bogies to Train");
+        System.out.println("====================================");
+
+        List<String> passengerBogies = new ArrayList<>();
+
+        passengerBogies.add("Sleeper");
+        passengerBogies.add("AC Chair");
+        passengerBogies.add("First Class");
+
+        System.out.println();
+        System.out.println("After Adding Bogies:");
+        System.out.println("Passenger Bogies : " + passengerBogies);
+
+        passengerBogies.remove("AC Chair");
+
+        System.out.println();
+        System.out.println("After Removing 'AC Chair':");
+        System.out.println("Passenger Bogies : " + passengerBogies);
+
+        System.out.println();
+        System.out.println("Checking if 'Sleeper' exists:");
+        System.out.println("Contains Sleeper? : " + passengerBogies.contains("Sleeper"));
+
+        System.out.println();
+        System.out.println("Final Train Passenger Consist:");
+        System.out.println(passengerBogies);
+
+        System.out.println();
+        System.out.println("UC2 operations completed successfully...");
+    }
 }
